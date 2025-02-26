d4e101778649f59262ec89828838109a480c0377
AceArnold <a.mutara@alustudent.com>
2025-02-26 08:48:51 +0000

    experiencial 4

---
+++ yourinput.py
@@ -0,0 +1,10 @@
+def userinput():
+    print("Enter your first number: ")
+    a = int(input())
+    print("Enter your second number: ")
+    b = int(input())
+    c = a + b
+    print("The result is: ", c)
+    return c
+
+userinput()  # Function call1
